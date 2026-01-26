0152628cc30b8140e2e430d0304bcf863813e2ec
temuritsutskiridze <temuri.tsutskiridze.04@gmail.com>
2026-01-26 05:32:23 +0400

    set and get data from redit

---
+++ src/currency/cache.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { createClient } from 'redis';
+
+@Injectable()
+export class CacheService {
+  private client;
+
+  constructor() {
+    this.client = createClient({
+      host: 'localhost',
+      port: 6379,
+    });
+    this.client.connect();
+  }
+
+  async get(key: string) {
+    try {
+      return await this.client.get(key);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async set(key: string, value: string, ttl: number) {
+    try {
+      await this.client.setEx(key, ttl, value);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
